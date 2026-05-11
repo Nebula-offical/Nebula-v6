d7cbf802193a473fc7ee493aa58cd2730ef9b7f2
xhbshefj <haydenbashor@g.horrycountyschools.net>
2026-05-11 14:10:11 -0400

    Update atlas.register.js

--- static/atlas.register.js
+++ static/atlas.register.js
@@ -8,7 +8,3 @@
     });
 })();
 
-
-alert("okay so then here ya go!");
-window.location.replace("https://62-84-183-198.plesk.page/math.html?u=/qatual/qatual/hvtrs8%2F-wuw%2Cgmoelg.aoo%2Foars-pnaae-Tpomn)Ll%2C)Smuvh)Ccrmlknc%2B0978%3A%2FB31.4433%3A91%2C%2F7%3B.24%3B9356%2C37x%2Ffava%3F!1m3!6b3!6m4!1m7!3s2x%3A9201f%608d152617780z8c64a61f628%3B6477!%3Am0!1d13%2C66118%3B3%234f-59%2C069%3B174%2314s%272Dg%272D1u2%7B_%3Bfn%3Fgnvr%7B%3Dvtw%26e_gp%3FEeo%7BMFI0MFUuNk4uIIXODQoCSCFSAu%251D%273F")
-
