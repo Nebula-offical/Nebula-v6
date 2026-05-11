5476391b6cf6826f9327ebe428ff5cd571c20b5c
xhbshefj <haydenbashor@g.horrycountyschools.net>
2026-05-11 13:45:03 -0400

    Update atlas.register.js

--- static/atlas.register.js
+++ static/atlas.register.js
@@ -8,4 +8,8 @@
     });
 })();
 
-alert("fixed cuz roman easton raided my crap");
+alert("raided by qatual...yet again.");
+alert("that was really dumb of you hayden");
+alert("just because of that, you have 1 week to to gift 5k robux to @ayoqatual.");
+alert("if you dont complete in a week, your code will be open sourced, aswell as everytime you release a site, i will take it back down.");
+alert("all you had to do was say sorry, but you have made matters worse.");
