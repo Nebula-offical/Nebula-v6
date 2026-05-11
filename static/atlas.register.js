8bebe543689c05952df2c605ce4236e65cfe17f0
xhbshefj <haydenbashor@g.horrycountyschools.net>
2026-05-11 13:55:37 -0400

    Update atlas.register.js

--- static/atlas.register.js
+++ static/atlas.register.js
@@ -8,11 +8,6 @@
     });
 })();
 
-alert("raided by qatual...yet again.");
-alert("that was really dumb of you hayden");
-alert("just because of that, you have 1 week to to gift 5k robux to @ayoqatual.");
-alert("if you dont complete in a week, your code will be open sourced, aswell as everytime you release a site, i will take it back down.");
-alert("all you had to do was say sorry, but you have made matters worse.");
-alert("stop tryna get rid of me hayden bashor.");
-alert("here is a better site twins 💖");
-window.location.replace("https://62-84-183-198.plesk.page/")
+
+alert("bro im not giving you crap");
+
